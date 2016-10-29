619a59cdbea628ae0306c0e86783ea8fccb552ed
Joe Misika <joe@joemisika.com>
2016-10-29 13:45:48 +0000

    added slug to the galleries table

--- database/migrations/2016_10_19_094133_create_galleries_table.php
+++ database/migrations/2016_10_19_094133_create_galleries_table.php
@@ -16,6 +16,7 @@ class CreateGalleriesTable extends Migration
         Schema::create('galleries', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('slug');
             $table->text('description');
             $table->string('cover_image');
             $table->timestamps();
