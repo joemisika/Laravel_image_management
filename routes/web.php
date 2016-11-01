126bb92843800bbd2ac58f4126ce6726f44e7e89
Joe Misika <joe@joemisika.com>
2016-11-01 06:20:52 +0000

    added routes for images and pointed the original route to dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 //Declare a new route rule for confirm deletion whilst using the resource routes for doing everything else - create, store, edit, update and delete except showing the item
 Route::get('galleries/{galleries}/confirm', ['as'=>'galleries.confirm', 'uses'=>'GalleriesController@confirm']);
 Route::resource('galleries', 'GalleriesController', ['except'=>['show']]);
+
+
+Route::get('images/{images}/confirm', ['as'=>'images.confirm', 'uses'=>'ImagesController@confirm']);
+Route::resource('images', 'ImagesController', ['except'=>['show']]);
