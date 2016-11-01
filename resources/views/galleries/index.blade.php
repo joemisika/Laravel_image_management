55a1feb52c691a79ff34df016abadedfdcd20c16
Joe Misika <joe@joemisika.com>
2016-11-01 10:05:22 +0000

    created the table to hold all the galleries

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -3,5 +3,39 @@
 @section('title', '')
 
 @section('content')
-
+    <a href="{{ route('galleries.create') }}">Create New Gallery</a>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <td>Title</td>
+            <td>Slug</td>
+            <td>Cover Image</td>
+            <td>Edit</td>
+            <td>Delete</td>
+        </tr>
+        </thead>
+        <tbody>
+        @if($galleries->isEmpty())
+            <tr>
+                <td colspan="5" align="center">There are no galleries</td>
+            </tr>
+        @else
+            @foreach($galleries as $gallery)
+        <tr>
+            <td>{{ $gallery->title }}</td>
+            <td>{{ $gallery->slug }}</td>
+            <td>
+                <img src="" alt="">
+            </td>
+            <td>
+                <a href="{{ route('galleries.edit'.$gallery->id) }}">Edit</a>
+            </td>
+            <td>
+                <a href="{{ route('galleries.confirm'.$gallery->id) }}">Delete</a>
+            </td>
+        </tr>
+            @endforeach
+         @endif
+        </tbody>
+    </table>
 @endsection
