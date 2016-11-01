96657ab8b1d15700ebafbd4c5ce7fa374061392d
Joe Misika <joe@joemisika.com>
2016-11-01 10:05:47 +0000

    created the form to add/edit galleries

---
+++ resources/views/galleries/form.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('title', '')
+
+@section('content')
+
+    {!! Form::model($gallery, [
+        'class'=>'form-horizontal',
+        'method'=> $gallery->exists ? 'put' : 'post',
+        'route'=>$gallery->exists ? ['galleries.update', $gallery->id]: ['galleries.store'], 'files'=>true ])
+    !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class'=>'form-control', 'id'=>'name']) !!}
+    </div>
+
+    @if($gallery->cover_image)
+       {{ Html::image($gallery->cover_image, '', array('class'=>'img-responsive')) }}
+    @endif
+
+    <div class="form-group">
+        {{ Form::file('cover_image') }}
+    </div>
+    
+    <div class="form-group">
+        {!! Form::label('description', 'Description:') !!}
+        {!! Form::textarea('description', null, ['class'=>'form-control', 'id'=>'description']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit($gallery->exists ? 'Save Gallery' : 'Create Gallery', ['class'=>'btn btn-primary']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+
+
+
+
+@endsection
