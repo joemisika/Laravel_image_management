010a7e284dc1983ed5bd1fc020ceb8328b4b530e
Joe Misika <joe@joemisika.com>
2016-11-01 07:12:04 +0000

    removed some ul and just commented the errors

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - Site Title</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 <nav class="navbar navbar-static-top navbar-inverse">
@@ -21,16 +22,6 @@
                 <a href="{{ route('images.index') }}">Images</a>
             </li>
         </ul>
-        {{--<ul class="nav navbar-nav navbar-right">--}}
-            {{--<?php--}}
-            {{--$greetings = array('Bonjour', 'Sawubona', 'Dumela', 'Jambo');--}}
-            {{--$v = array_rand($greetings);--}}
-            {{--$greeting = $greetings[$v];--}}
-            {{--//echo $greeting;--}}
-            {{--?>--}}
-            {{--<li><span class="navbar-text"><?php echo $greeting; ?>, {{ Auth::user()->firstname }} </span></li>--}}
-            {{--<li><a href="/logout">Logout</a></li>--}}
-        {{--</ul>--}}
     </div>
 </nav>
 <div class="container">
@@ -38,9 +29,9 @@
         <div class="col-lg-12">
             <h3>@yield('title')</h3>
 
-            @include('common.errors')
+            {{--@include('common.errors')--}}
 
-            @include('common.status')
+            {{--@include('common.status')--}}
 
             @yield('content')
 
