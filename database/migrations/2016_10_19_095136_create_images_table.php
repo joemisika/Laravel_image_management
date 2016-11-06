388d6b8b562802f1ed080130ba613c98b6c26f71
Joe Misika <joe@joemisika.com>
2016-11-06 15:43:35 +0000

    changed the table to include upload_path and image values

--- database/migrations/2016_10_19_095136_create_images_table.php
+++ database/migrations/2016_10_19_095136_create_images_table.php
@@ -16,6 +16,7 @@ class CreateImagesTable extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('gallery_id')->unsigned();
+            $table->string('upload_path');
             $table->string('image');
             $table->string('title');
             $table->text('description');
