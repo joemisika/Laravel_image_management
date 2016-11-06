fc1eb9f76bd7e54beb7e93dbb3833116d621107b
Joe Misika <joe@joemisika.com>
2016-11-06 15:41:30 +0000

    added status view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\View\Composers;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['view']->composer(['layouts.main'], Composers\AddStatusMessage::class);
     }
 
     /**
