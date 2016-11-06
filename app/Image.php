0c60a78bc3836d87feb21ffd824e699a7fe936f2
Joe Misika <joe@joemisika.com>
2016-11-06 16:13:50 +0000

    added upload_path to the fillables

--- app/Image.php
+++ app/Image.php
@@ -8,5 +8,10 @@ class Image extends Model
 {
     protected $table = 'images';
 
-    protected $fillable = ['gallery_id', 'image', 'title', 'description', 'image_credit'];
+    protected $fillable = ['gallery_id', 'upload_path', 'image', 'title', 'description', 'image_credit'];
+
+//    public function Gallery()
+//    {
+//        $this->belongsTo(Gallery::class);
+//    }
 }
