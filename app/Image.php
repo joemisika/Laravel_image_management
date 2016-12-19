8b038d161c3b8995bf8485988d42bdff601a1596
Joe Misika <joe@joemisika.com>
2016-12-19 16:35:04 +0000

    defined the relationship

--- app/Image.php
+++ app/Image.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    protected $table = 'images';
+   protected $table = 'images';
 
-    protected $fillable = ['gallery_id', 'upload_path', 'image', 'title', 'description', 'image_credit'];
+   protected $fillable = ['gallery_id', 'upload_path', 'image', 'title', 'description', 'image_credit'];
 
-//    public function Gallery()
-//    {
-//        $this->belongsTo(Gallery::class);
-//    }
+   public function Gallery()
+   {
+      return $this->belongsTo(Gallery::class);
+   }
 }
