59e4e234903b445397fb1669bf1ce0ba38603877
Joe Misika <joe@joemisika.com>
2016-12-19 16:35:40 +0000

    tweaked the function that calls all the images

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-use App\Image;
 use App\Gallery;
+use App\Image;
 use App\Http\Requests\StoreImageRequest;
 use App\Http\Requests\UpdateImageRequest;
 
@@ -16,28 +16,23 @@ use App\Helpers\FileUploads;
 class ImagesController extends Controller
 {
 
-    protected $images;
+   protected $images;
 
-    public function __construct(Image $images)
-    {
-        $this->images = $images;
+   public function __construct(Image $images)
+   {
+      $this->images = $images;
+   }
 
-        //parent::__construct();
-    }
-
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-        $images = $this->images->with('Images')->get();
-
-        return view('images.index', compact('images'));
-    }
+   /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+   public function index()
+   {
+      $images = $this->images->with('Gallery')->get();
+      return view('images.index', compact('images'));
+   }
 
    /**
    * Show the form for creating a new resource.
@@ -69,13 +64,14 @@ class ImagesController extends Controller
          $image->image = $result[1].$result[0];
       }
 
-      $image->name = $request->title;
+      $image->gallery_id = $request->gallery_id;
+      $image->title = $request->title;
       $image->description = $request->description;
       $image->image_credit = $request->image_credit;
       $image->save();
 
-        //redirect back to the form after successfully add the information
-      return redirect('galleries.create')->with('status', 'gallery has been created successfully!!!');
+      //redirect back to the form after successfully add the information
+      return redirect(route('images.create'))->with('status', 'image has been created successfully!!!');
    }
 
     /**
@@ -84,24 +80,24 @@ class ImagesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+   public function show($id)
+   {
         //
-    }
+   }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $image = $this->images->findorFail($id);
-        $galleries = $this->getGalleries();
+   /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+   public function edit($id)
+   {
+      $image = $this->images->findorFail($id);
+      $galleries = $this->getGalleries();
 
-        return view('galleries.form', compact('image', 'galleries'));
-    }
+      return view('galleries.form', compact('image', 'galleries'));
+   }
 
     /**
      * Update the specified resource in storage.
@@ -110,56 +106,55 @@ class ImagesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateImageRequest $request, $id)
-    {
-        $image = new Image();
-        if($request->hasFile('image'))
-        {
-            $newUpload = new FileUploads();
-            //get $result back from FileUploads Helper
-            $result = $newUpload->uploadFile($request->file('image'));
-            //upload path plus name of image
-            $image->image = $result[1].$result[0];
-        }
+   public function update(UpdateImageRequest $request, $id)
+   {
+      $image = new Image();
+      if($request->hasFile('image'))
+      {
+         $newUpload = new FileUploads();
+         //get $result back from FileUploads Helper
+         $result = $newUpload->uploadFile($request->file('image'));
+         //upload path plus name of image
+         $image->image = $result[1].$result[0];
+      }
 
-        $image->name = $request->title;
-        $image->description = $request->description;
-        $image->image_credit = $request->image_credit;
-        $image->save();
+      $image->name = $request->title;
+      $image->description = $request->description;
+      $image->image_credit = $request->image_credit;
+      $image->save();
 
-        //redirect back to the edit after successfully editing the information
-        return redirect(route('backend.destinations.edit', $image->id))
+      //redirect back to the edit after successfully editing the information
+      return redirect(route('backend.destinations.edit', $image->id))
             ->with('status', 'Image edited successfully!');
-    }
+   }
 
-    /**
-     * Confirm that you want to delete specified resource
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function confirm($id)
-    {
-        $image = $this->images->findOrFail($id);
+   /**
+   * Confirm that you want to delete specified resource
+   *
+   * @param int $id
+   * @return \Illuminate\Http\Response
+   */
+   public function confirm($id)
+   {
+      $image = $this->images->findOrFail($id);
+      return view('images.confirm', compact('image'));
+   }
 
-        return view('images.confirm', compact('image'));
-    }
+   /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+   public function destroy($id)
+   {
+      $image = $this->images->findOrFail($id);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $image = $this->images->findOrFail($id);
+      $image->delete();
 
-        $image->delete();
-
-        return redirect(route('images.index'))
+      return redirect(route('images.index'))
             ->with('status', 'Image has been deleted');
-    }
+   }
 
    /*
    * Create a lists of available galleries
