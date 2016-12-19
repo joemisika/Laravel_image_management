8902a5f4315b9f58bc0e2443880f00252a6e8b3d
Joe Misika <joe@joemisika.com>
2016-12-19 15:01:25 +0000

    added the form to upload content

--- resources/views/images/form.blade.php
+++ resources/views/images/form.blade.php
@@ -4,21 +4,39 @@
 
 @section('content')
 
-    {!! Form::model($image, [
-        'class'=>'form-horizontal',
-        'method'=> $image->exists ? 'put' : 'post',
-        'route'=>$image->exists ? ['images.update', $image->id]: ['images.store'], 'files'=>true ])
-    !!}
+   {!! Form::model($image, ['class'=>'form-horizontal','method'=> $image->exists ? 'put' : 'post','route'=>$image->exists ? ['images.update', $image->id]: ['images.store'], 'files'=>true ])!!}
 
-    <div class="form-group">
-        {!! Form::label('name', 'Name:') !!}
-        {!! Form::text('name', null, ['class'=>'form-control', 'id'=>'name']) !!}
+    	<div class="form-group">
+        	{!! Form::label('title', 'Title:') !!}
+      	{!! Form::text('title', null, ['class'=>'form-control', 'id'=>'title']) !!}
+    	</div>
+    	<div class="form-group">
+        {{-- <div class="col-md-3"> --}}
+            {!! Form::label('gallery_id', 'Galleries') !!}
+            {!! Form::select('gallery_id', $galleries, null, ['class' => 'form-control']) !!}
+        {{-- </div> --}}
     </div>
 
+		@if($image->cover_image)
+       	{{ Html::image($image->image, '', array('class'=>'img-responsive')) }}
+    	@endif
+		<div class="form-group">
+      	{{ Form::file('image') }}
+    	</div>
 
-    <div class="form-group">
-        {!! Form::submit($image->exists ? 'Save Image' : 'Create Image', ['class'=>'btn btn-primary']) !!}
-    </div>
+		<div class="form-group">
+        	{!! Form::label('image_credit', 'Image Credit:') !!}
+        	{!! Form::textarea('image_credit', null, ['class'=>'form-control', 'id'=>'image_credit']) !!}
+    	</div>
 
-    {!! Form::close() !!}
+    	<div class="form-group">
+        	{!! Form::label('description', 'Description:') !!}
+        	{!! Form::textarea('description', null, ['class'=>'form-control', 'id'=>'description']) !!}
+    	</div>
+
+    	<div class="form-group">
+      	{!! Form::submit($image->exists ? 'Save Image' : 'Create Image', ['class'=>'btn btn-primary']) !!}
+    	</div>
+
+   {!! Form::close() !!}
 @endsection
